9b818b2cf5aff53c9b96a33acded8cb1f3a4d01d
chuckie82 <chuckie82@gmail.com>
2022-12-10 08:07:32 -0800

    add difference image and clean up

--- ptycho_reconstruct.py
+++ ptycho_reconstruct.py
@@ -1,25 +1,18 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import cupy as cp
-
+import copy
 import h5py
 import matplotlib.pyplot as plt
-
-# from Operators import Split_plan, Overlap_plan #, Project_data
-from Operators import Split_Overlap_plan  # , Project_data
-import Solvers
-
-Alternating_projections = Solvers.Alternating_projections
-
-
-from Operators import get_times, reset_times, normalize_times
-
-reset_times()
-
 from timeit import default_timer as timer
 
+from Operators import Split_Overlap_plan
+import Solvers
+from Operators import get_times, reset_times, normalize_times
 import config
 
+Alternating_projections = Solvers.Alternating_projections
+reset_times()
 GPU = config.GPU
 
 if GPU:
@@ -58,7 +51,6 @@ translations_x = xp.real(translations)
 translations_y = xp.imag(translations)
 
 # get the image extent (with wrap-around)
-# Nx,Ny = truth.shape
 Nx = xp.int(xp.ceil(xp.max(translations_x) - xp.min(translations_x)))
 Ny = Nx
 
@@ -97,9 +89,6 @@ if GPU:
     print("data size", data.nbytes)
     print("----")
 
-# Split = Split_plan(translations_x,translations_y,nx,ny,Nx,Ny)
-# Overlap = Overlap_plan(translations_x,translations_y,nx,ny,Nx,Ny)
-
 img_initial = xp.ones((Nx, Ny), dtype=xp.complex64)
 ############################
 # reconstruct
@@ -111,7 +100,7 @@ print(
     "not refining illumination, starting with good one, maxiter:",
     maxiter,
 )
-#residuals_interval = np.inf
+# residuals_interval = np.inf
 residuals_interval = 10
 
 
@@ -128,35 +117,12 @@ img4, frames, illum, residuals_AP = Alternating_projections(
 )
 print("total time:", timer() - t0)
 
-#%%
-
-"""
-
-=======
->>>>>>> e7496afc69877ad027d0898523ae5e0f03a11448
-lens_true=xp.fft.fft2(xp.fft.fftshift(illumination))
-lens_init=xp.abs(lens_true) # remove true phase
-illum_init=xp.fft.fftshift(xp.fft.ifft2(lens_init))
-
-
-img3,frames, residuals_AP3 = Alternating_projections(img_initial, illumination, Overlap, Split, data, refine_illumination = False, maxiter = maxiter, img_truth = truth)
-
-print('not refining illumination, starting with the wrong one')
-img3,frames, residuals_AP3 = Alternating_projections(img_initial, illum_init, Overlap, Split, data, refine_illumination = False, maxiter = maxiter, img_truth = truth)
-
-
-print('refining illumination')
-img2,frames, residuals_AP2 = Alternating_projections(img_initial, illum_init, Overlap, Split, data, refine_illumination = True, maxiter = maxiter, img_truth = truth)
-
-"""
-
 ############################
 
 
 ## print results
 timers = get_times()
 
-import copy
 
 tt_ops = copy.deepcopy(timers)
 tt_solver = copy.deepcopy(Solvers.get_times())
@@ -206,47 +172,45 @@ print(
 
 # calculate mse
 nrm0 = xp.linalg.norm(truth)
-# nmse4=mse_calc(truth,img4)/nrm0
 if residuals_AP.size > 0:
     nmse4 = residuals_AP[-1, 0]
 else:
     nmse4 = np.NaN
 
-fig, axs = plt.subplots(nrows=2, ncols=1, sharex=True, figsize=(10, 10))
-
 if GPU:
     truth = truth.get()
     img = img4.get()
     residuals_AP = residuals_AP.get()
-
 else:
     img = img4
 
-
-axs[0].set_title("Truth", fontsize=10)
-axs[0].imshow(abs(truth), cmap="gray")
-axs[1].set_title("Alternating Projections no Sync:%2.2g" % (nmse4), fontsize=10)
-axs[1].imshow(abs(img), cmap="gray")
-
+fig = plt.figure(figsize=(10, 10))
+plt.subplot(1, 3, 1)
+plt.title("Ground truth", fontsize=10)
+plt.imshow(abs(truth), cmap="gray")
+plt.colorbar()
+plt.subplot(1, 3, 2)
+plt.title("Alternating Projections\n no Sync:%2.2g" % (nmse4), fontsize=10)
+plt.imshow(abs(img), cmap="gray")
+plt.colorbar()
+plt.subplot(1, 3, 3)
+plt.title("Difference")
+plt.imshow(abs(truth) - abs(img), cmap="jet")
+plt.colorbar()
 plt.show()
 
 ##
 
 # make a new figure with residuals
 fig, axs = plt.subplots(nrows=3, ncols=1, sharex=True, figsize=(10, 10))
-
 axs[0].semilogy(residuals_AP[:, 0])
 axs[0].set_title("|img-f truth| (f=phase scalar)")
 axs[1].semilogy(residuals_AP[:, 1])
 axs[1].set_title("||frames|-data|")
-
 axs[2].semilogy(residuals_AP[:, 2])
 axs[2].set_title("frames overlapped")
+plt.show()
 
-
-# tt_solver['solver_tot']
-# tt_solver['loop']
-#%%
 print(
     "solver tot (seconds) :",
     tt_solver["solver_tot"],
